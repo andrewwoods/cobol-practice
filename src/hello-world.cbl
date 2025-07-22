2e204309e44bb6960867540ad0e7aec70c8afe68
Andrew Woods <services@awoods.net>
2025-07-22 17:20:17 -0400

    Create hello world in COBOL
    This is the simplest program one can write.

---
+++ src/hello-world.cbl
@@ -0,0 +1,10 @@
+      * 
+      * Purpose: To perform the great programming ritual of Hello World. 
+      *
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. hello-world.
+       AUTHOR. Andrew-Woods.
+       DATE-WRITTEN. 2025-07-22.
+       PROCEDURE DIVISION.
+           DISPLAY "Hello World!".
+           STOP RUN.
