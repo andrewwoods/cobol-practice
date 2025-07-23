34fa1cdf98fbc9cacc84cc28187bb2a3445d0c40
Andrew Woods <services@awoods.net>
2025-07-23 16:26:49 -0400

    Update fizzbuzz to match test data
    COBOL is strict about the width of a number based on the digits in the
    PIC clause. In this case PIC 99 displays a two-digit number i.e. "16"
    normally, but a single-digit number i.e. 7 would display as " 7" in the
    output. So I had to create a PIC clause for each width, because TRIM
    didn't work as I was expecting. I probably called it incorrectly.

--- src/fizzbuzz.cbl
+++ src/fizzbuzz.cbl
@@ -6,19 +6,24 @@
        AUTHOR. Andrew-Woods.
        DATE-WRITTEN. 2025-07-22.
 
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       REPOSITORY.
+           FUNCTION ALL INTRINSIC.
+
        DATA DIVISION.
        WORKING-STORAGE SECTION.
-       01 WS-COUNTER PIC 9(2) VALUE 1.
+       01 WS-COUNTER PIC 99 VALUE 1.
+       01 WS-INCR-1-DIGIT PIC 9.
+       01 WS-INCR-2-DIGIT PIC 99.
        01 WS-BY-3 PIC 9(1).
        01 WS-BY-5 PIC 9(1).
        01 WS-CALC PIC 9(2).
       * 
        PROCEDURE DIVISION.
-           DISPLAY "Doing FizzBuzz!".
-           PERFORM 16 TIMES
-      D        DISPLAY "WS-COUNTER=" WS-COUNTER
+           PERFORM VARYING WS-COUNTER FROM 1 BY 1 UNTIL WS-COUNTER >= 16
+
                PERFORM 1000-FIZZBUZZ
-               COMPUTE WS-COUNTER = WS-COUNTER + 1
            END-PERFORM.
            STOP RUN.
 
@@ -26,13 +31,20 @@
            DIVIDE WS-COUNTER BY 3 GIVING WS-CALC REMAINDER WS-BY-3
            DIVIDE WS-COUNTER BY 5 GIVING WS-CALC REMAINDER WS-BY-5
 
-           IF WS-BY-3 = 0 AND WS-BY-5 = 0 
-               DISPLAY "FizzBuzz:" WS-COUNTER
-           ELSE IF WS-BY-3 = 0 
-               DISPLAY "Fizz:" WS-COUNTER
-           ELSE IF WS-BY-5 = 0 
-               DISPLAY "Buzz:" WS-COUNTER
+           MOVE FUNCTION TRIM(WS-COUNTER LEADING) TO WS-INCR-1-DIGIT.
+           MOVE FUNCTION TRIM(WS-COUNTER LEADING) TO WS-INCR-2-DIGIT.
+
+           IF WS-BY-3 = 0 AND WS-BY-5 = 0
+               DISPLAY "Fizz Buzz"
+           ELSE IF WS-BY-3 = 0
+               DISPLAY "Fizz"
+           ELSE IF WS-BY-5 = 0
+               DISPLAY "Buzz"
            ELSE
-               DISPLAY WS-COUNTER
+               IF WS-COUNTER < 10
+                   DISPLAY WS-INCR-1-DIGIT
+               ELSE
+                   DISPLAY WS-INCR-2-DIGIT
+               END-IF
            END-IF.
 
